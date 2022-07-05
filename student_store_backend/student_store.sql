1f82bd83e2776b66957814a77c541ef31dc4ea04
hayley <simmons.hayley15@icloud.com>
2022-07-05 04:58:36 -0500

    Renamed SQL scripts; added new default script

---
+++ student_store_backend/student_store.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate Student Store database?'
+\prompt 'Press ENTER for yes, or CTRL-C to cancel: > ' answer
+
+DROP DATABASE student_store;
+CREATE DATABASE student_store;
+
+\connect student_store
+\i student_store_schema.sql
+
+
+-- \echo 'Delete and recreate Student Store database?'
+-- \prompt 'Press ENTER for yes, or CTRL-C to cancel: > ' answer
+
+-- DROP DATABASE student_store_test;
+-- CREATE DATABASE student_store_test;
+
+-- \connect student_store_test
+-- \i student_store_schema.sql
